a62d37e4603f7fdb3adba3ef5ff37ed5a7577521
convivial-system-4430 <cdneal091@gmail.com>
2019-01-25 06:04:32 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,3 +5,9 @@ function updatePlaylist(object, artist, title)
   object[artist] = title;
   return (object);
 }
+
+function removeFromPlaylist(object, artist)
+{
+  delete object.artist;
+  return(object);
+}
